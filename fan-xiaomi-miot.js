e501f2951bb8733604d112474b7a0ad55e995eea
kylin <kylin_fedora@hotmail.com>
2022-08-12 22:56:15 +0800

    Update fan-xiaomi-miot.js

--- fan-xiaomi-miot.js
+++ fan-xiaomi-miot.js
@@ -463,7 +463,7 @@ class fanXiaomiMiotCard extends LitElement {
         display: grid;
         grid-template-rows: var(--card--grid-rows, min-content, 30px, 30px, 30px);
         grid-template-columns: var(--card--grid-columns, repeat(5, 1fr));
-        grid-template-areas: var(--card--grid-areas, "n n n n n" "p sp sd hs ha" "lock o m vs va" "bc al t h bc");
+        grid-template-areas: var(--card--grid-areas, "n n n n n" "p sp sd hs ha" "lock o m vs va" "bc al t h p");
         gap: var(--card-grid-gap, 6px 6px);
         align-items: center;
         place-content: space-between;
